e92d8f0daf0a13a842b1c56548608401c2ba7f22
dangdung2352637 <108926988+dangdung2352637@users.noreply.github.com>
2022-09-15 20:06:32 +0700

    up

---
+++ bottradingview.js
@@ -0,0 +1,8 @@
+const hideAds = setInterval(() => {
+    const adDialog = document.querySelector("div[data-dialog-name='gopro']");
+    if (adDialog)
+    {
+        adDialog.querySelector("button").click();
+        console.log('popup closed');
+    }
+  }, 10);
